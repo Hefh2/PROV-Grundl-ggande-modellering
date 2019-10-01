b5c3289841369e744a30e4c9903b3b425af61cbb
Hefh2 <henning.joelsson@elev.ga.ntig.se>
2019-10-01 12:54:00 +0200

    Added variables and method "NewBook"

--- Prov Modellering/Prov Modellering/Book.cs
+++ Prov Modellering/Prov Modellering/Book.cs
@@ -8,6 +8,17 @@ namespace Prov_Modellering
 {
     class Book
     {
+        public int price;
+        string name;
+        int rarity;
+        string category;
+        int actualValue;
+        bool cursed;
+        static Random generator = new Random();
 
+        public void Book(string name)
+        {
+
+        }
     }
 }
