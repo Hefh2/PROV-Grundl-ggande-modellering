ac54c4e1771a4e8fc083d8214dcf8e6780ef9783
Hefh2 <henning.joelsson@elev.ga.ntig.se>
2019-10-01 12:55:16 +0200

    Mend for last commit
    Forgot to save before commiting and don't know how to mend on the github client.

--- Prov Modellering/Prov Modellering/Book.cs
+++ Prov Modellering/Prov Modellering/Book.cs
@@ -9,14 +9,53 @@ namespace Prov_Modellering
     class Book
     {
         public int price;
-        string name;
         int rarity;
+        string name;
         string category;
         int actualValue;
         bool cursed;
         static Random generator = new Random();
 
-        public void Book(string name)
+        public void NewBook(List<string> names, List<string> categories)
+        {
+            //Checks length of lists and randomizes a name and category to give the new book.
+            int namesLength = names.Count;
+            int categoriesLength = categories.Count;
+
+            name = names[generator.Next(1, namesLength)];
+            category = categories[generator.Next(1, categoriesLength)];
+
+            actualValue = generator.Next(100, 1001);
+            rarity = generator.Next(1, 5);
+            int i = generator.Next(1, 5);
+            if (i == 4)
+            {
+                cursed = true;
+            }
+
+        }
+
+        public void PrintInfo()
+        {
+
+        }
+
+        public int Evaluate()
+        {
+
+        }
+
+        public string GetCategory()
+        {
+
+        }
+
+        public string GetName()
+        {
+
+        }
+
+        public bool GetCursed()
         {
 
         }
